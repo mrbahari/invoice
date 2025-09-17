6d34f3ad3efa32258ccd6145bf73725a1e77b2a0
Mr Bahari <mrbahari.ir@gmail.com>
2025-09-17 08:20:23 +0000

    مشکل حا نشد

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,9 @@
 import { LoginForm } from '@/components/auth/login-form';
-import { AuthProvider } from '@/components/auth/auth-provider';
 
 export default function LoginPage() {
   return (
-    <AuthProvider>
-        <div className="flex min-h-screen items-center justify-center bg-muted/40">
-            <LoginForm />
-        </div>
-    </AuthProvider>
+    <div className="flex min-h-screen items-center justify-center bg-muted/40">
+        <LoginForm />
+    </div>
   );
 }

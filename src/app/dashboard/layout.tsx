33806c31c4e0267bea44b5c0d32969b91cf75f01
Mr Bahari <mrbahari.ir@gmail.com>
2025-09-18 14:21:10 +0000

    یه ایکون بعلاوه کوچک اومده اون وسط ولی اصلا دیده نمیشه و جلوه خوبی نداره

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -39,7 +39,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
     <SearchProvider>
       <div className="flex min-h-screen w-full flex-col bg-muted/40 pb-24 sm:pb-0">
         <SidebarNav activeTab={activeTab} onTabChange={handleTabChange} />
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pr-14">
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
           <Header activeTab={activeTab} onTabChange={handleTabChange} />
           <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
              {children}

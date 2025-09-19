83345635fb589247db96c14d2b68c078fe129c3b
Mr Bahari <mrbahari.ir@gmail.com>
2025-09-19 09:02:26 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,9 +7,8 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // This is a mock configuration. Replace it with your actual Firebase config.
 const firebaseConfig = {
-  "projectId": "studio-5061951288-49d1h",
+  "projectId": "studio-5061951288-49d18",
   "appId": "1:187985328856:web:76a7c5fe18f29ce67735ad",
-  "storageBucket": "studio-5061951288-49d18.appspot.com",
   "apiKey": "AIzaSyBr46llypgYD3fDEOTb5v-AF4jQpa_xTq8",
   "authDomain": "studio-5061951288-49d18.firebaseapp.com",
   "messagingSenderId": "187985328856"

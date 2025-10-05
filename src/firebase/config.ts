ba1e8919b278c452c4434dd7c19fd9f3757ae493
Mr Bahari <mrbahari.ir@gmail.com>
2025-10-05 18:36:36 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-5061951288-49d18",
-  "appId": "1:187985328856:web:76a7c5fe18f29ce67735ad",
-  "storageBucket": "studio-5061951288-49d18.appspot.com",
-  "apiKey": "AIzaSyBr46llypgYD3fDEOTb5v-AF4jQpa_xTq8",
-  "authDomain": "studio-5061951288-49d18.firebaseapp.com",
-  "messagingSenderId": "187985328856"
+  "projectId": "hisaabgarv1-2-52613231-274bb",
+  "appId": "1:482949235305:web:2adb12d7e41b464beb98f8",
+  "apiKey": "AIzaSyClLDkjVOq1LuXCWhdqZEVBErmuQDLiVYA",
+  "authDomain": "hisaabgarv1-2-52613231-274bb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "482949235305"
 };
